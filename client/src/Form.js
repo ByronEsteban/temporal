1e429f69f8ef2aa2550d77ef3d87c4f2a506911e
AntezanaValentin <valentinescabj@gmail.com>
2022-11-08 12:47:27 -0300

    mejora react

--- client/src/Form.js
+++ client/src/Form.js
@@ -32,57 +32,59 @@ const Form = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+    <div id="myCarousel" className="carousel slide text-bg-dark" data-bs-ride="carousel">
+        <div className="carousel-indicators text-bg-dark">
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="active" aria-current="true"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className=""></button>
         </div>
-        <div class="carousel-inner text-bg-">
-          <div class="carousel-item">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+        <div className="carousel-inner text-bg-dark">
+          <div className="carousel-item">
+            <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
 
-            <div class="container">
-              <div class="carousel-caption text-start text-bg-dark">
-                <h1>deberia estar arriba</h1>
+            <div className="container">
+              <div className="carousel-caption text-start text-bg-dark">
                 <p className = "text-center text-bg-dark fs-3">Matematicas</p>
-                <p><a class="btn btn-lg btn-primary" href="#">Jugar</a></p>
+                <p className="text-center">
+                <p className="justify-content-center text-align-center"><a className="btn btn-lg btn-primary" href="#">Jugar</a></p>
+                </p>
               </div>
             </div>
           </div>
-          <div class="carousel-item active">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+          <div className="carousel-item active">
+            <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
 
-            <div class="container">
-              <div class="carousel-caption">
+            <div className="container">
+              <div className="carousel-caption">
                 <h1>Preguntados la35</h1>
                 <p>Para jugar y elegir el tema de las preguntas o preguntar pase la pagina</p>
 
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+          <div className="carousel-item">
+            <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
 
-            <div class="container">
-              <div class="carousel-caption text-end">
-                <h1>One more for good measure.</h1>
+            <div className="container">
+              <div className="carousel-caption text-end">
+              <p className="text-center">
                 <p className="text-center fs-3">Ajedrez</p>
-                <p><a class="btn btn-lg btn-dark" href="#">Jugar</a></p>
+                <p><a className="btn btn-lg btn-primary btn btn-lg btn-primary" href="#">Jugar</a></p>
+              </p>
               </div>
             </div>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+        <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+        <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
-    </form>
+    </form >
   );
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -8,10 +8,12 @@ const header = () => {
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
          <ul class="text-center">
+         <p className="text-center">
           <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li><a href="#" class="nav-link px-2 text-white fs-2">Preguntados</a></li>
-            <li><a href="#" class="btn btn-warning fs-2">La35</a></li>
+            <li><a class="text-bg-warning fs-2">La35</a></li>
             </ul>
+            </p>
           </ul>
           <div class="text-end">
 
